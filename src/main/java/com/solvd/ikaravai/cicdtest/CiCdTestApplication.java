d688eda3e7b4ca0d22598eaf017f9c2ac8f725e0
Ivan Karavai <ikaravai@solvd.com>
2022-08-26 19:03:28 +0300

    Add placeholder content

--- src/main/java/com/solvd/ikaravai/cicdtest/CiCdTestApplication.java
+++ src/main/java/com/solvd/ikaravai/cicdtest/CiCdTestApplication.java
@@ -1,12 +1,22 @@
 package com.solvd.ikaravai.cicdtest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 public class CiCdTestApplication {
 
+    @PostConstruct
+    public void method() {
+        log.info("--- App constructed ---");
+    }
+
     public static void main(String[] args) {
+        log.info("--- App execution starting ---");
         SpringApplication.run(CiCdTestApplication.class, args);
     }
 
--- src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
@@ -1,13 +1,21 @@
 package com.solvd.ikaravai.cicdtest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiCdTestApplicationTests {
 
+    public static Logger log = LoggerFactory.getLogger(CiCdTestApplicationTests.class);
+
     @Test
     void contextLoads() {
+        log.info(() -> "--- Executing tests ---");
+        assertEquals(2, 1 + 1);
     }
 
 }
