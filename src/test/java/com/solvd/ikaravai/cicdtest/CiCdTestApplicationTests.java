498f8a2d53ac1dc08e56aca06d86a36096f3b17b
Ivan Karavai <ikaravai@solvd.com>
2022-08-26 18:46:07 +0300

    First commit

---
+++ src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvd.ikaravai.cicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
