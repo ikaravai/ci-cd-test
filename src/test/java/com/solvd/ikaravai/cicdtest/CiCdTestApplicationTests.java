556fb0379e3268700c75751c819848759b66ed1b
Ivan Karavai <ikaravai@solvd.com>
2022-08-29 12:07:10 +0300

    Edit dummy test to trigger jenkins build and push to dockerhub

--- src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
@@ -29,7 +29,7 @@ class CiCdTestApplicationTests {
 
     @Test
     void dummyTest() {
-        log.info(() -> "--- Executing Updated good Dummy TEST ---");
+        log.info(() -> "--- Executing Updated old Dummy TEST ---");
         Random rand = new Random();
         var x = rand.nextInt(10);
         assertTrue(x <= 100);
