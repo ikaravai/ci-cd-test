9279ea396a9245a44c52fb7ba5dc99ac7defa92a
Ivan Karavai <ikaravai@solvd.com>
2022-08-29 11:26:04 +0300

    Edit dummy test to trigger jenkins build

--- src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
+++ src/test/java/com/solvd/ikaravai/cicdtest/CiCdTestApplicationTests.java
@@ -29,7 +29,7 @@ class CiCdTestApplicationTests {
 
     @Test
     void dummyTest() {
-        log.info(() -> "--- Executing Updated easy Dummy TEST ---");
+        log.info(() -> "--- Executing Updated weird Dummy TEST ---");
         Random rand = new Random();
         var x = rand.nextInt(10);
         assertTrue(x <= 100);
